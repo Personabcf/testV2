92a764223130a7a7caa26acf67fc93890f97b6c0
Personabcf <jjpigott@waimea.school.nz>
2025-05-15 10:23:56 +1200

    Update streamlit_codev2.py

--- streamlit_codev2.py
+++ streamlit_codev2.py
@@ -68,7 +68,7 @@ if not daily_data.empty:
     st.line_chart(daily_data, x="Hour", y="Temperature")
 else:
     st.warning("No data for selected day")
-st.warning("This is a warning")
+st.error("This is a ERROR")
 """
 df["Date"] = pd.to_datetime(
     df["Date"].str.strip(),
